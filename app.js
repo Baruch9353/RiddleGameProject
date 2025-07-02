49f2f3a9e344a02007f20e40c0944f493145f7fc
baruch9353 <baruchminzberg@gmail.com>
2025-07-02 16:34:30 +0300

    dun

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ import Player from "./classes/Player.js";
 import riddles from "./riddles/riddlesArr.js";
 import timeWrapper from "./utils/timer.js";
 
-// Loop over the riddles
-for (const riddle of riddles) {
-  timeWrapper(() => riddle.ask(), player);
-}
-
 // Starting the game
 const playerName = question("What is your name? ");
 console.log(`Hi ${playerName}!\nWelcome to the Riddle Game!`);
 
 const player = new Player(playerName);
+
+// Loop over the riddles
+for (const riddle of riddles) {
+  timeWrapper(() => riddle.ask(), player);
+}
 
 player.showStats();
